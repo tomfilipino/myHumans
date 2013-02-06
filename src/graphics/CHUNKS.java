735601ab4c593312e134b6ad805e02d2d68b22c9
tomfilipino <ehf.colombo@gmail.com>
2013-02-06 10:26:32 -0200

    real chunks

--- src/graphics/CHUNKS.java
+++ src/graphics/CHUNKS.java
@@ -1,5 +1,7 @@
 package graphics;
 
+import java.awt.geom.Point2D;
+
 import enviroment.Cell;
 import game.screen.Screen;
 
@@ -17,65 +19,23 @@ public class CHUNKS {
 	
 	private static int i;
 	private static int index;
-	private static int bufferSIZE = 128;
-	private static int cacheSIZE=1000;
-//	public static Pixmap buffer = new Pixmap(bufferSIZE,bufferSIZE,Format.RGBA4444);
-	public static Array<SpriteCache> chunks = new Array<SpriteCache>(0);
+	private static int chunkSIZE=256;
+	public static Pixmap buffer = new Pixmap(bufferSIZE,bufferSIZE,Format.RGBA4444);
+	public static Array<chunk> chunkList = new Array<chunk>(0);
 	public static Array<Integer> cacheID = new Array<Integer>(0);
 	private static Matrix4 projection = Screen.spriteBatch.getProjectionMatrix();	
 	
-	
-	
-	public static void ADDcache(TextureRegion T, float x, float y){		
-		if(index==0&&i==0){
-			BEGINcache();	
-		}
-		chunks.get(index).add(T,x,y);
-		i++;
-		if(i==cacheSIZE){
-			ENDcache();
-			i=0;
-			index++;
-			BEGINcache();
+	static class chunk{
+		Point2D.Float pos;
+		Pixmap data = new Pixmap(chunkSIZE,chunkSIZE,Pixmap.Format.RGBA4444);		
+		public chunk(Point2D.Float p, Pixmap pixmap){
+			pos = new Point2D.Float(p.x, p.y);
+			data = pixmap;
 		}
 	}
+	
+	
+	
 
-	public static void BEGINcache(){		
-		chunks.add(new SpriteCache());
-		chunks.get(index).beginCache();
-		chunks.get(index).setProjectionMatrix(projection);
-	}
-	
-	
-	public static void ENDcache(){			
-		cacheID.add(chunks.get(index).endCache());	
-	}
-	public static void CLOSEcache(){	
-		cacheID.add(chunks.get(index).endCache());
-	}
-	
-	
-	public static void CLEARcache() {
-		i=0;
-		index=0;
-		cacheID.clear();
-		for(int i=0;i<chunks.size;i++){
-			chunks.get(i).clear();
-		}
-		chunks.clear();		
-		projection = Screen.spriteBatch.getProjectionMatrix();			
-	}
-	public static void RENDERcache() {
-		Gdx.gl.glEnable(GL10.GL_BLEND);
-		//Gdx.app.log("RENDER INFO", "> " + chunks.size + " " + cacheID.size + " <");
-
-		for(int i=0;i<chunks.size;i++){
-			int id = cacheID.get(i);
-			chunks.get(i).begin();
-			chunks.get(i).draw(id);
-			chunks.get(i).end();
-		}
-	}
-	
 
 }
